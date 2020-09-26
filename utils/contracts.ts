1e1d21aa8d9673c1e4382f06a5eca0a22387321f
bweick <brian.weickmann@gmail.com>
2020-09-25 21:10:01 -0600

    Added contracts.

---
+++ utils/contracts.ts
@@ -0,0 +1,4 @@
+export { IndexDao } from "../typechain/IndexDAO";
+export { MerkleDistributor } from "../typechain/MerkleDistributor";
+export { RewardsDistributionRecipient } from "../typechain/RewardsDistributionRecipient";
+export { StakingRewards } from "../typechain/StakingRewards";
